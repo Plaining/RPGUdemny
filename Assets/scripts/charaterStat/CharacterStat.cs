35bac7c67a6f88beb3f6e0c692545b7d3a3eb530
Plaining <d1078029483@163.com>
2025-09-27 16:45:26 +0800

    1

--- Assets/scripts/charaterStat/CharacterStat.cs
+++ Assets/scripts/charaterStat/CharacterStat.cs
@@ -6,21 +6,37 @@ public class CharacterStat : MonoBehaviour
 {
     [Header("Major stats")]
     public Stat strength;// power! 1 point increase damage by 1 and crit.power by 1%
-    public Stat agility; // movespeed! 1 point increase evasion by 1% and crit.chance by 1%
+    public Stat agility; // minjie! 1 point increase evasion by 1% and crit.chance by 1%
     public Stat intelligence; // naozi! 1 point increase magic damage by 1 and magic resistance by 3%
     public Stat vitality; // huoli! 1 point increase health by 3 or 5 point
+
+    [Header("Offensive stats")]
+    public Stat critChange;//baojilv
+    public Stat critPower;//baojishanghai
+    public Stat damage;
 
     [Header("Defensive stats")]
     public Stat maxHealth;
     public Stat evasion; //shanbi shanbi
     public Stat armor; // wuqi jianshang
+    public Stat magicRegistance;
 
-    public Stat damage;
+    [Header("Magic stats")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat lightningDamage;
+
+    public bool isIgnited;
+    public bool isChilled;
+    public bool isShocked;
+
+
     [SerializeField] private int currentHealth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public virtual void Start()
     {
+        critPower.SetDefaultValue(150);
         currentHealth = maxHealth.getValue();
     }
 
@@ -31,8 +47,66 @@ public class CharacterStat : MonoBehaviour
             return;
         }
         int totalDamage = damage.getValue() + strength.getValue();
+        if (CanCrit())
+        {
+            totalDamage = CalculateCriticalDamage(totalDamage);
+        }
         totalDamage = CheckTargetArmor(_targetStat, totalDamage);
-        _targetStat.TakeDamage(totalDamage);
+        Debug.Log("totalDamage: " + totalDamage);
+        //_targetStat.TakeDamage(totalDamage);
+        DoDamicalDamage(_targetStat);
+    }
+
+    public virtual void DoDamicalDamage(CharacterStat _targetStat)
+    {
+        int _fireDamage = fireDamage.getValue();
+        int _iceDamage = iceDamage.getValue();
+        int _lightningDamage = lightningDamage.getValue();
+        int totalMagicalDamage = _fireDamage + _iceDamage + _lightningDamage + intelligence.getValue();
+        totalMagicalDamage = CheckTargetResistance(_targetStat, totalMagicalDamage);
+        _targetStat.TakeDamage(totalMagicalDamage);
+
+        if(Mathf.Max(_fireDamage,_iceDamage,_lightningDamage)<=0)
+        {
+            return;
+        }
+
+        bool canApplyIgnite = _fireDamage > _iceDamage && _iceDamage > _lightningDamage;
+        bool canApplyChill = _iceDamage > _fireDamage && _iceDamage > _lightningDamage;
+        bool canApplyShock = _lightningDamage > _fireDamage && _lightningDamage > _iceDamage;
+
+
+        while(!canApplyChill && !canApplyShock && !canApplyIgnite)
+        {
+            if (Random.value < .5f && _fireDamage > 0)
+            {
+                canApplyIgnite = true;
+                _targetStat.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                return;
+            }
+            if (Random.value < .5f && _iceDamage > 0)
+            {
+                canApplyChill = true;
+                _targetStat.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                return;
+            }
+            if (Random.value < .5f && _lightningDamage > 0)
+            {
+                canApplyIgnite = true;
+                _targetStat.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                return;
+            }
+
+        }
+
+        _targetStat.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock); 
+    }
+
+    private static int CheckTargetResistance(CharacterStat _targetStat, int totalMagicalDamage)
+    {
+        totalMagicalDamage -= _targetStat.magicRegistance.getValue() + (_targetStat.intelligence.getValue() * 3);
+        totalMagicalDamage = Mathf.Clamp(totalMagicalDamage, 0, int.MaxValue);
+        return totalMagicalDamage;
     }
 
     private static int CheckTargetArmor(CharacterStat _targetStat, int totalDamage)
@@ -54,6 +128,16 @@ public class CharacterStat : MonoBehaviour
         return false;
     }
 
+    public void ApplyAilments(bool _ignite, bool _chill, bool _shock)
+    {
+        if (isIgnited || isChilled || isShocked) {
+            return;
+        }
+        isIgnited = _ignite;
+        isChilled = _chill;
+        isShocked = _shock;
+    }
+
     public virtual void TakeDamage(int _damage)
     {
         currentHealth -= _damage;
@@ -66,5 +150,25 @@ public class CharacterStat : MonoBehaviour
     protected virtual void Die()
     {
         //throw new NotImplementedException();
+    }
+
+    private bool CanCrit()
+    {
+        int totalCriticalChance = critChange.getValue() + agility.getValue();
+        if (Random.Range(0,100)<= totalCriticalChance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private int CalculateCriticalDamage(int _damage)
+    {
+        Debug.Log("pre critical damage:" + critPower.getValue() + _damage);//baoshang
+        float totalCritPower = (critPower.getValue() + strength.getValue()) * .01f;
+        float critDamage = _damage * totalCritPower;
+        Debug.Log("critical damage:" + critDamage);
+        return Mathf.RoundToInt(critDamage);
+
     }
 }
