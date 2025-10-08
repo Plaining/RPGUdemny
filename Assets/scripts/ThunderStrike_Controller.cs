f6b7fe29100e13794a043197f304f0f2258a71d9
Plaining <d1078029483@163.com>
2025-10-08 18:28:34 +0800

    11

---
+++ Assets/scripts/ThunderStrike_Controller.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ThunderStrike_Controller : MonoBehaviour
+{
+    [SerializeField] private CharacterStat targetStats;
+    [SerializeField] private float speed;
+
+    private Animator anim;
+    private bool triggered;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered)
+        {
+            return;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetStats.transform.position, speed * Time.deltaTime);
+        transform.right = transform.position - targetStats.transform.position;
+        
+        if(Vector2.Distance(transform.position, targetStats.transform.position) < .1f)
+        {
+            anim.transform.localPosition = new Vector3(0, .5f);
+            anim.transform.localRotation = Quaternion.identity;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = new Vector3(2, 2);
+
+            triggered = true;
+            anim.SetTrigger("Hit");
+            Invoke("DamageAndSelfDestroy", .2f);
+        }
+    }
+
+    private void DamageAndSelfDestroy()
+    {
+        targetStats.TakeDamage(1);
+        Destroy(gameObject, .4f);
+    }
+}
